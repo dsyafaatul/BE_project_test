1ddc1d38cf520535df160bd8e9780006308372a0
D. Syafa'atul Anbiya <dikisyafaatul@gmail.com>
2024-10-23 21:22:13 +0700

    enable cors credentials

--- src/app.js
+++ src/app.js
@@ -12,7 +12,8 @@ const cors = require('cors')
 app.use(cors({
     origin: [
         'http://localhost:5173'
-    ]
+    ],
+    credentials: true
 }))
 app.use(express.json())
 app.use(cookieParser(process.env.SECRET_KEY))
